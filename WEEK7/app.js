df71f1f3e9f80e1379f37d93b2f630dbb83a6409
Kitsanapong-F <09chaisu@gmail.com>
2026-02-09 21:16:57 +0700

    app

---
+++ WEEK7/app.js
@@ -0,0 +1,5 @@
+  function show(){
+            alert("สวัสดี! นี่คือข้อตวามจาก java script");
+        }
+        console.log("hello java script");
+        document.getElementById("btn").addEventListener("click",show);
